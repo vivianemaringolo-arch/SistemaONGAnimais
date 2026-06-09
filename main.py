b14df5b3ffecfda26d6c95c176ff82c67273606e
vivianemaringolo-arch <viviane.maringolo@sou.unifeob.edu.br>
2026-06-08 22:01:43 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
-print("=== Sistema ONG Animais ===")
+import streamlit as st
 
-while True:
+st.set_page_config(
+    page_title="ONG Animais",
+    page_icon="🐾",
+    layout="wide"
+)
 
-    print("\n1 - Cadastro de animais")
-    print("2 - Sair")
+st.title("🐾 Sistema de Gestão da ONG")
 
-    opcao = input("Escolha uma opção: ")
+st.write("Bem-vinda ao sistema da ONG.")
 
-    if opcao == "1":
-        print("Módulo de cadastro")
-    elif opcao == "2":
-        break
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric("Animais Abrigados", 0)
+
+with col2:
+    st.metric("Adoções", 0)
+
+with col3:
+    st.metric("Vacinas Pendentes", 0)
+
+st.divider()
+
+st.write("Selecione uma opção no menu lateral.")
