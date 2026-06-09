c43c93f63ca8af5de41c454560319c054cdb0fe6
vivianemaringolo-arch <viviane.maringolo@sou.unifeob.edu.br>
2026-06-08 22:00:03 -0300

    Atualizar o main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Sistema ONG Animais")
+print("=== Sistema ONG Animais ===")
+
+while True:
+
+    print("\n1 - Cadastro de animais")
+    print("2 - Sair")
+
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == "1":
+        print("Módulo de cadastro")
+    elif opcao == "2":
+        break
